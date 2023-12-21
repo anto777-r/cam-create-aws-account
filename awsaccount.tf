df1ca6eba3fecb031138262703e7116e704ae60b
anto777-r <154500344+anto777-r@users.noreply.github.com>
2023-12-21 13:12:37 +0530

    Create awsaccount.tf

---
+++ awsaccount.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.0"
+    }
+  }
+}
+
+provider "aws" {
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+  region     = var.region
+}
+
+resource "aws_organizations_account" "account" {
+  email = var.root_email
+  name = var.account_name
+  tags = var.tags
+  close_on_deletion = true
+}
+
