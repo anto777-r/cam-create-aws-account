f3a9731fdf4d0503b31edd6853f50e8cf7c9685b
anto777-r <154500344+anto777-r@users.noreply.github.com>
2023-12-21 13:13:14 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "region" {
+  type = string
+  default = "us-east-1"
+}
+
+variable "account_name" {
+  type = string
+  description = "(Required) Account Name"
+}
+
+variable "root_email" {
+  type = string
+  description = "(Required) Account Email"
+}
+
+variable tags{
+  type = map(string)
+  description = "(Required) Tags for the resource"
+}
+
+variable AWS_ACCESS_KEY_ID {}
+variable AWS_SECRET_ACCESS_KEY {}
